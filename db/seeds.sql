82134e87e664ca4a00d1c972304009f6299319db
mytoehertz <juanitosemail@gmail.com>
2019-05-14 15:49:54 -0400

    uploading app

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers_table
+    (burger_name)
+VALUES("Sloppy");
+
+INSERT INTO burgers_table
+    (burger_name)
+VALUES("Bacon Double");
+
+INSERT INTO burgers_table
+    (burger_name)
+VALUES("Veggie");
